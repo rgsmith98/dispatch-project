a25c5130edf382e08d3af1841fd7b7d515e51617
ranparagaurav <ranparagaurav98@gmail.com>
2020-03-18 19:30:35 +0530

    Complete Splash Screen

---
+++ app/src/main/java/com/example/dispatchproject/activity_splash.java
@@ -0,0 +1,39 @@
+package com.example.dispatchproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class activity_splash extends AppCompatActivity {
+    Animation anim;
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_view);
+        imageView=(ImageView)findViewById(R.id.splash_logo); // Declare an imageView to show the animation.
+        anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in); // Create the animation.
+        anim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity(new Intent(activity_splash.this, MainActivity.class));
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        imageView.startAnimation(anim);
+    }
+}
