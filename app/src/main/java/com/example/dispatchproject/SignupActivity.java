fb815f281e3d5e467ed705759f2775f0515fd45d
ranparagaurav <ranparagaurav98@gmail.com>
2020-03-22 11:56:57 +0530

    Complete Design of Signup Screen

--- app/src/main/java/com/example/dispatchproject/SignupActivity.java
+++ app/src/main/java/com/example/dispatchproject/SignupActivity.java
@@ -1,12 +1,20 @@
 package com.example.dispatchproject;
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,21 +22,70 @@ public class SignupActivity extends AppCompatActivity {
 
     //Account Type Spinner
     Spinner accountType;
+    EditText emailAddress;
     Context context = this;
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        accountType = findViewById(R.id.registerType);
+        /**
+         => Initialize Account Type Spinner and Set Data into it
+         --------------------------------------------------------
+        **/
 
-        //Set Data in Array For Account Type
-        String[] accontTypeValues = { "Production", "Supply", "Quality", "Dispatch" };
+                accountType = findViewById(R.id.registerType);
 
-        //Set Data in Adapter
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(context, R.layout.spinner_layout, accontTypeValues);
+                accountType.setFocusable(true);
+                accountType.setFocusableInTouchMode(true);
 
-        accountType.setAdapter(adapter);
+                //Set Data in Array For Account Type
+                String[] accontTypeValues = { "Select Account Type", "Production", "Supply", "Quality", "Dispatch" };
+
+                //Set Data in Adapter
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(context, R.layout.spinner_layout, accontTypeValues);
+
+                //By Default Select First Value
+                accountType.setSelection(0);
+
+                //Set Adapter in Spinner
+                accountType.setAdapter(adapter);
+        /**
+         ------------------------------------------------
+        **/
+
+
+
+        /**
+            => Initialize Email Address Field
+         ------------------------------------------------
+        **/
+                emailAddress = findViewById(R.id.emailInput);
+        /**
+         ------------------------------------------------
+        **/
+    }
+
+
+
+    //Hide Keyboard on outside touch
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if ( v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event);
     }
 }
